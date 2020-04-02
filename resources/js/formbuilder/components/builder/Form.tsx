a8ba517ec4fd4a935ebe0b2c9d8556c3c017158e
robert <robyeyco@gmail.com>
2020-04-02 23:50:32 +0700

    Added Select comoponent for previous form reference

--- resources/js/formbuilder/components/builder/Form.tsx
+++ resources/js/formbuilder/components/builder/Form.tsx
@@ -39,12 +39,13 @@ export default class Form extends React.Component<Props, State>{
       formData: {},
       currentIndex: 0,
     };    
-    DescriptionField.defaultProps = {updateFormDescription: this.updateFormDescription};
-    TitleField.defaultProps = {updateFormTitle: this.updateFormTitle};
-    EditableField.defaultProps = {addField: this.addField, switchField: this.swapFields,
+    DescriptionField.defaultProps = { updateFormDescription: this.updateFormDescription };
+    TitleField.defaultProps = { updateFormTitle: this.updateFormTitle };
+    EditableField.defaultProps = { addField: this.addField, switchField: this.swapFields,
                                   removeField: this.removeField, updateField: this.updateField,
                                   renameField: this.renameField, insertField: this.insertField,
-                                  swapFields: this.swapFields};
+                                  swapFields: this.swapFields };
+      TextField.defaultProps ={ getTagList: this.getTagList };
     
   }
 
@@ -169,6 +170,24 @@ export default class Form extends React.Component<Props, State>{
     order[idxSource] = target;
     order[idxTarget] = source;
     this.setState({ uiSchema, error: null });
+  }
+
+  getTagList = (): any[] =>{
+    return [{ val:"1",
+              label:"Form1",
+              items:[
+                { parentVal:1, val:"question1", label:"Active Lisenting" },
+                { parentVal:1, val:"question2", label:"Firday" },
+                { parentVal:1, val:"question3", label:"Saturday" }]
+            },
+            { val:"2",
+              label:"Form2",
+              items:[
+                { parentVal:1, val:"question4", label:"Monday" },
+                { parentVal:1, val:"question5", label:"Tuesday" },
+                { parentVal:1, val:"question6", label:"Thursday" }]
+            }
+          ];
   }
 
   //********  Render *******/
--- resources/js/formbuilder/components/fields/TextField.tsx
+++ resources/js/formbuilder/components/fields/TextField.tsx
@@ -1,9 +1,10 @@
-import * as React from "react";
-import { EditorState, convertToRaw, convertFromRaw } from 'draft-js';
+import * as React from 'react';
+import { EditorState, convertToRaw, convertFromRaw, Modifier } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 import ReactHtmlParser from 'react-html-parser';
+import SxSelect from '../common/nestedselect/SxSelect';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './css/fields.css';
 
@@ -33,7 +34,10 @@ export class RichText extends  React.Component<any,any>{
 }
 
 export class TextField extends React.Component<any,any>{
-    
+    public static defaultProps = {
+      getTagList: (): any[] => { return [{}];}
+    };
+  
     constructor(props) {
         super(props);
         const { schema } = props;        
@@ -64,12 +68,13 @@ export class TextField extends React.Component<any,any>{
     render(){
         const { editorState } = this.state;
         return (
-          <div>
+          <div className="rich-editor-root">
             <Editor
               editorState={editorState}
               wrapperClassName="rich-editor-wrapper"
               editorClassName="rich-editor-textarea"
               onEditorStateChange={this.onEditorStateChange}
+              toolbarCustomButtons={[<TagOption tagList={this.props.getTagList()} />]}
             />
             {/*<textarea
               disabled
@@ -80,3 +85,32 @@ export class TextField extends React.Component<any,any>{
     }
 }
 
+interface TagOptionProps{
+    tagList: [any];
+    editorState: EditorState;
+    onChange: (value: EditorState) => void;
+}
+
+class TagOption extends React.Component<any, any> {
+    
+  onSelectChange = (item)=>{
+    const { editorState, onChange } = this.props;
+    const contentState = Modifier.replaceText(
+      editorState.getCurrentContent(),
+      editorState.getSelection(),
+      `#${item.parentVal}:#${item.val}`,
+      editorState.getCurrentInlineStyle(),
+    );
+    onChange(EditorState.push(editorState, contentState, 'insert-characters'));
+  }
+
+  render() {
+    return (
+      <div>
+        <SxSelect
+          placeholder="Form/Question" list={this.props.tagList}
+          onSelectChange={this.onSelectChange}/>
+      </div>
+    );
+  }
+}
