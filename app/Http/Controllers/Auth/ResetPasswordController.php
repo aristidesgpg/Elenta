4b5b7cc8e5892a8eca953264f7ef9d3651c44a98
d-taranets <dmytrotaranec@gmail.com>
2020-04-13 12:25:06 +0300

    Minor fixes

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -23,6 +23,21 @@ class ResetPasswordController extends Controller
 
     use ResetsPasswords;
 
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request, $token = null)
+    {
+        return view('index');
+    }
+
     /**
      * Where to redirect users after resetting their password.
      *
