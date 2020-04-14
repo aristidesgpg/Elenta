8989453c0456a65dffb63baca6637a3ef04298f9
Alex Pham <alex.pham468@gmail.com>
2020-04-14 17:18:32 +1000

    added return type for tags

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,29 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Tag extends Model
 {
     /**
      * Get all of the templates that are assigned this tag.
      */
-    public function templates() {
+    public function templates(): MorphToMany {
         return $this->morphedByMany(Template::class, 'taggable');
     }
 
     /**
      * Get all of the programs that are assigned this tag.
      */
-    //TODO: need to add the programs(): MorphMany return type thing, unsure what it is
-    public function programs() {
+    public function programs(): MorphToMany {
         return $this->morphedByMany(Program::class, 'taggable');
     }
 
     /**
      * Get all of the modules that are assigned this tag.
      */
-    public function modules() {
+    public function modules(): MorphToMany {
         return $this->morphedByMany(Module::class, 'taggable');
     }
 }
