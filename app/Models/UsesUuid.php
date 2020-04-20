8ce62be9d35a3453d5c547b42b19cc5e0466dbdb
Anshul Jain <ajnatural@gmail.com>
2020-04-20 12:12:18 +1000

    Fix UsesUuid

--- app/Models/UsesUuid.php
+++ app/Models/UsesUuid.php
@@ -1,13 +1,13 @@
 <?php
 namespace App\Models;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 trait UsesUuid
 {
     protected static function bootUsesUuid()
     {
-        static::creating(function (BaseModel $model) {
+        static::creating(function (Model $model) {
             if (!$model->getKey()) {
                 $model->{$model->getKeyName()} = (string) Str::uuid();
             }
