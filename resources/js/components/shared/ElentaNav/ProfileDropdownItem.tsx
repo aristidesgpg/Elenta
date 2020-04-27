ab9e137dbc59bb934062737b5cbf5eed687ba2ca
Anshul Jain <ajnatural@gmail.com>
2020-04-27 18:14:20 +1000

    UI cleanup for ModuleEditor, change nav dropdown to send to preferences

--- resources/js/components/shared/ElentaNav/ProfileDropdownItem.tsx
+++ resources/js/components/shared/ElentaNav/ProfileDropdownItem.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+import { useHistory } from "react-router-dom";
 
 export const ProfileDropdownItem = ({profile, onClick}) => {
+  const history = useHistory();
   return (
-    <Dropdown.Item onClick={onClick}>
-      ({profile.__typename == "ConsultantProfile" ? "Consultant" : "Learner"}) {profile.title || profile.role}
+    <Dropdown.Item onClick={() => history.push('/preferences')}>
+      Preferences
     </Dropdown.Item>
   );
 };
